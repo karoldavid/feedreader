40d4656efb5bedbb7e503f26647754f6c321d9b0
lorak <k.zysk@zoho.com>
2015-08-21 14:58:05 +0200

    add test 'allFeeds have url and url is not empty'

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -26,12 +26,17 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 
-
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
 
+        it('have URL', function() {
+            allFeeds.forEach(function(feed){
+                expect(feed.url).toBeDefined();
+                expect(feed.url.length).not.toBe(0);
+            });
+        });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
