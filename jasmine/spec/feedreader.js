30f594082680693e04ee79cb4151d62fec76f5bd
lorak <k.zysk@zoho.com>
2015-08-21 15:10:00 +0200

    add test suite 'Menu Element', 'Feed Entry', 'Feed Content'

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -52,7 +52,7 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "The menu" */
-
+    describe('Menu Element', function() {
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
@@ -64,20 +64,22 @@ $(function() {
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+    });
 
     /* TODO: Write a new test suite named "Initial Entries" */
-
+    describe('Feed Entry', function() {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test wil require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
-
-    /* TODO: Write a new test suite named "New Feed Selection"
-
+    });
+    /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('Feed Content', function() {
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+    });
 }());
