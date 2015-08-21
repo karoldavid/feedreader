fb3fa6d0b4ef967a537e7476c4f39fbb7f17e209
lorak <k.zysk@zoho.com>
2015-08-21 15:00:59 +0200

    add test 'allFeeds have name and name is not empty'

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,7 +31,7 @@ $(function() {
          * and that the URL is not empty.
          */
 
-        it('have URL', function() {
+        it('have url', function() {
             allFeeds.forEach(function(feed){
                 expect(feed.url).toBeDefined();
                 expect(feed.url.length).not.toBe(0);
@@ -42,6 +42,12 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('have name', function() {
+            allFeeds.forEach(function(feed){
+                expect(feed.name).toBeDefined();
+                expect(feed.name.length).not.toBe(0);
+            });
+        });
     });
 
 
