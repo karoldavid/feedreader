5008c7b4df7deed98b3cefbf53a8ca65b04e6da7
lorak <k.zysk@zoho.com>
2015-08-22 23:22:44 +0200

    add test suites 'Initial Entries' and 'New Feed Selection'

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -87,12 +87,29 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test wil require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+
+        beforeEach(function(done) {
+            loadFeed(0, done);
+        });
+
+        it("have at least 1 entry", function() {
+            expect($('.feed .entry').length).not.toBe(0);
+        });
     });
+
     /* TODO: Write a new test suite named "New Feed Selection" */
     describe('New Feed Selection', function() {
-        /* TODO: Write a test that ensures when a new feed is loaded
+    	 /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
-    });
+        beforeEach(function(done) {
+            loadFeed(1, done);
+        });
+
+        it('has different content', function() {
+            expect($('.header-title').html()).not.toBe(allFeeds[0].name);
+        });
+
+     });
 }());
