def3e186024278b5f80a06f5c36c57790d570de6
lorak <k.zysk@zoho.com>
2015-08-22 22:12:13 +0200

    add menu test 'is hidden' 'toggles on click'

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -52,22 +52,35 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "The menu" */
-    describe('Menu Element', function() {
+    describe('The Menu', function() {
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+        it('is hidden', function() {
+            expect($('body.menu-hidden').length).toBe(1);
+        });
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+        it('toggles on click', function() {
+
+            $('.menu-icon-link').click();
+            expect($('body.menu-hidden').length).toBe(0);
+
+            $('.menu-icon-link').click();
+            expect($('body.menu-hidden').length).toBe(1);
+
+        });
+
     });
 
     /* TODO: Write a new test suite named "Initial Entries" */
-    describe('Feed Entry', function() {
+    describe('Initial Entries', function() {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
@@ -76,7 +89,7 @@ $(function() {
          */
     });
     /* TODO: Write a new test suite named "New Feed Selection" */
-    describe('Feed Content', function() {
+    describe('New Feed Selection', function() {
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
