c2a5fb38e52c76cce50b0be1b09817ec20fe32ce
lorak <k.zysk@zoho.com>
2015-08-26 17:48:40 +0200

    modify test suite New Feed Selection

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -79,7 +79,7 @@ $(function() {
 
     });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+   /* TODO: Write a new test suite named "Initial Entries" */
     describe('Initial Entries', function() {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -103,12 +103,18 @@ $(function() {
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+        var title = $('.header-title').html();
+
         beforeEach(function(done) {
             loadFeed(1, done);
         });
 
+        afterEach(function(done) {
+            loadFeed(0, done); // reset feed to default
+        });
+
         it('has different content', function() {
-            expect($('.header-title').html()).not.toBe(allFeeds[0].name);
+            expect($('.header-title').html()).not.toBe(title);
         });
 
      });
